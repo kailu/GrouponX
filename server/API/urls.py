7b207096867f96e6a318976839098b79a18bf77e
kai lu <kailu2009@yahoo.com>
2014-08-08 22:40:19 -0700

    initial urls settings

---
+++ server/API/urls.py
@@ -0,0 +1,14 @@
+
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+
+import views
+
+urlpatterns = patterns('',
+    url(r'^index/', views.index, name='index' ),
+    url(r'^configure/', views.configure, name="configure"),
+    url(r'^register/', views.configure, name="register"),
+    url(r'^createPageID', views.createPageID, name="createPageID"),
+
+)
--- server/API/views.py
+++ server/API/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+
+def index(request):
+    return HttpResponse("hello, world!")
+
+
+@login_required(login_url='/accounts/login/')
+def configure(request):
+    """
+    
+    Arguments:
+    - `request`:
+    """
+    pass
+
+
+def register(request):
+    """
+    
+    Arguments:
+    - `request`:
+    """
+    pass
+
+@login_required(login_url='/accounts/login/')
+def createPageID(request):
+    """
+    
+    Arguments:
+    - `request`:
+    """
+    pass
+
+
+
+
