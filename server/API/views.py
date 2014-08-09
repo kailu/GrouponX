7f591b0fcc243ba9f674a867cbd752ce3f89c09a
figo77ll <feih@yahoo-inc.com>
2014-08-09 12:25:06 -0700

    register to db works

--- server/API/views.py
+++ server/API/views.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from django.contrib.auth import authenticate
 
 from django.shortcuts import render
 from django.http import HttpResponse
@@ -6,6 +7,9 @@ from django.contrib.auth.decorators import login_required
 from django.template import RequestContext as RC
 
 from django.shortcuts import render, render_to_response
+from models import RegistrationForm
+from django.contrib.auth.models import User
+
 from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 
@@ -45,10 +49,27 @@ def login(request):
 
 
 def register(request):
-    return render_to_response(
-        'registration/registration_form.html',
-        {},
-        context_instance=RC(request, {}),
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password1']
+        new_user = User.objects.create_user(username, '', password)
+        new_user.save()
+        print "=== User: ", username, " password: ", password
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                print("User is valid, active and authenticated")
+            else:
+                print("The password is valid, but the account has been disabled!")
+        else:
+            # the authentication system was unable to verify the username and password
+            print("The username and password were incorrect.")
+    else:
+        form = RegistrationForm()
+        return render_to_response(
+            'registration/registration_form.html',
+            {'form' : form},
+            context_instance=RC(request, {}),
     )
 
 
